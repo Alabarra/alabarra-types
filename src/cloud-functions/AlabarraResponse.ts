9e8ecf8444a7bdcdf8aac94a9c9082f547917912
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-07-06 23:13:52 +0200

    Updates response type

--- src/cloud-functions/AlabarraResponse.ts
+++ src/cloud-functions/AlabarraResponse.ts
@@ -6,7 +6,12 @@ export enum AlabarraResponseStatus {
 /**
  * Represents the result of a call to the Create Order API
  */
-export interface AlabarraResponse {
+type AlabarraResponse = AlabarraResponseSuccess | AlabarraResponseError;
+
+/**
+ * Represents the result of a call to the Create Order API
+ */
+ export interface AlabarraResponseSuccess {
 
     /**
      * Whether the request succedded or failed
@@ -17,10 +22,20 @@ export interface AlabarraResponse {
      * Result in case of success
      */
     result?: any;
+}
+
+/**
+ * Represents the result of a call to the Create Order API
+ */
+ export interface AlabarraResponseError {
+
+    /**
+     * Whether the request succedded or failed
+     */
+    status: AlabarraResponseStatus;
 
     /**
      * Error message in case of error
      */
     error_message?: string;
-
 }
