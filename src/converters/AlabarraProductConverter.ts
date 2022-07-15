e21e2c0b59f74fbf751aeb9e249afb517d53254d
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-07-15 22:31:35 +0200

    updstes types

--- src/converters/AlabarraProductConverter.ts
+++ src/converters/AlabarraProductConverter.ts
@@ -10,7 +10,7 @@ export const ProductConverter: FirestoreDataConverter<AlabarraProduct> = {
 		if (newProduct.created_at != undefined) {
 			newProduct.created_at = newProduct.created_at
 		} else {
-			newProduct.created_at = new Date()
+			newProduct.created_at = serverTimestamp();
 		}
 		
 		return newProduct;
@@ -20,6 +20,12 @@ export const ProductConverter: FirestoreDataConverter<AlabarraProduct> = {
 		data.id = snapshot.id;
 		data.ref = snapshot.ref;
 		data.path = snapshot.ref.path;
+		if (data.created_at != undefined && data.created_at != null) {
+			data.created_at = (data.created_at as Timestamp).toDate()
+		}
+		if (data.last_updated_at != undefined && data.last_updated_at != null) {
+			data.last_updated_at = (data.created_at as Timestamp).toDate()
+		}
 		return data as AlabarraProduct;
 	},
 };
