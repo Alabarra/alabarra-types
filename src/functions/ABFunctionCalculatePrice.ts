df05114fa7fcf5c5835310d293b7408dd3e191da
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-11-20 22:47:32 +0100

    Updates calculate price function

--- src/functions/ABFunctionCalculatePrice.ts
+++ src/functions/ABFunctionCalculatePrice.ts
@@ -5,32 +5,34 @@ export const ABFunctionCalculatePrice = (product: ABProduct | ABProductData, sel
     let unitPrice = product.price;
 
     // Go trough every selected option
-    selectedOptions.forEach((_, index) => {
+    selectedOptions.forEach((selectedOption, index) => {
 
-        // Original product option
-        const productOption = product.options[index];
+        const optionId = selectedOption.option_id;
 
-        if (productOption.type == ABProductOptionsType.SINGLE_SELECTION) {
-            //Get selected option
-            const singleSelectedValue = selectedOptions[index] as ABProductOptionSingleSelectedValue;
-            
-            // Find product option that is selected to find price adjustment value
-            const selectedOption = productOption.possible_values.find((prod) => prod.id === singleSelectedValue.option_id);
-            if (selectedOption) {
-                unitPrice += selectedOption.price_adjustment;
-            }
-        } else if (productOption.type == ABProductOptionsType.MULTIPLE_SELECTION) {
-            
-            //Get selected option
-            const selectedValues = selectedOptions[index] as ABProductOptionMultipleSelectedValues;
+        const productOption = product.options.find((obj) => obj.id === optionId);
 
-            selectedValues.selected_values.forEach((selectedValue) => {
-                // Find product option that is selected to find price adjustment value
-                const selectedOption = productOption.possible_values.find((prod) => prod.id === selectedValue);
-                if (selectedOption) {
-                    unitPrice += selectedOption.price_adjustment;
+        if (productOption) {
+            if (productOption.type === ABProductOptionsType.SINGLE_SELECTION) {
+                // Cast selection appropiately
+                const singleSelectedOption = selectedOption as ABProductOptionSingleSelectedValue;
+                const selectedPossibleValue = productOption.possible_values.find(obj => obj.id === singleSelectedOption.option_id);
+                if (selectedPossibleValue) {
+                    unitPrice += selectedPossibleValue.price_adjustment;
                 }
-            });
+            } else if (productOption.type === ABProductOptionsType.MULTIPLE_SELECTION) {
+                // Cast selection appropiately
+                const multipleSelectedOption = selectedOption as ABProductOptionMultipleSelectedValues;
+                //const selectedPossibleValues = productOption.possible_values.find(obj => obj.id === multipleSelectedOption.option_id);
+
+
+                // Increase the price for every selected option
+                multipleSelectedOption.selected_values.forEach(selectedValueId => {
+                    const selectedPossibleValue = productOption.possible_values.find(obj => obj.id === selectedValueId);
+                    if (selectedPossibleValue) {
+                        unitPrice += selectedPossibleValue.price_adjustment;
+                    }
+                });
+            }
         }
     });
 
