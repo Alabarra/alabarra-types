f7021c65bdc791ba16051496ee53b073b02e32f1
Daniel Valenzuela <daniel.valenzuela@carbyte.de>
2022-12-05 00:20:32 +0100

    Extends stripe metadata

--- src/cloud-functions/ABStripeMetadata.ts
+++ src/cloud-functions/ABStripeMetadata.ts
@@ -15,4 +15,14 @@ export type ABStripeMetadata = {
      * id of the table, which is in turn its name
      */
     payment_id: string;
+
+    /**
+     * Bill total without tips or discounts
+     */
+    bill_total: number;
+
+    /**
+     * total tip
+     */
+    tip: number;
 }
